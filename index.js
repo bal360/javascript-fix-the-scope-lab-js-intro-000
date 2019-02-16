427391e5bb870c3bfab7e1100ea329d9a13d4d01
Blake A Long <blake.a.long@gmail.com>
2019-02-16 22:05:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 
 var funkyFunction = function theFunk() {
-  return funkyFunction()
+  return theFunk
 }
 
 
